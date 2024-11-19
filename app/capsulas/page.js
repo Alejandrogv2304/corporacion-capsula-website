326bf4b929e66a55f61f7bd2e2bc945e2919068c
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2024-11-18 23:24:59 -0500

    Solucionando bug de next

--- app/capsulas/page.js
+++ app/capsulas/page.js
@@ -20,9 +20,11 @@ export const metadata={
             <p className="text-primary-200 text-lg mb-10">
             Descubre una nueva experiencia de descanso en Bucaramanga con nuestras cápsulas de dormir al estilo japonés. Perfectas para viajeros, estudiantes y quienes buscan un refugio cómodo y privado, nuestras cápsulas están diseñadas para ofrecer un ambiente seguro, limpio y climatizado, con iluminación personal y acceso a WiFi. Disfruta de la comodidad y la privacidad de una cápsula moderna y relájate en el corazón de la ciudad. Ya sea para una siesta rápida o una noche completa, nuestras cápsulas son la opción ideal para recargar energías. 
             </p>
+
             <Suspense fallback={<Spinner/>}>
             <CapsulaList/>
             </Suspense>
+            
           </div>
          
           
