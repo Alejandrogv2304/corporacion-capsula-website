4d2521ba9e2c5c5223d528c683b505ef6d47fcd0
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2024-11-18 22:42:39 -0500

    Agregando la pagina de Capsula
    Agregué interfaz de Capsula para cadina página, rutas dinamicas.

--- app/_components/CapsulaCard.js
+++ app/_components/CapsulaCard.js
@@ -48,7 +48,7 @@ function CapsulaCard({ cabin }) {
 
         <div className="bg-primary-950 border-t border-t-primary-800 text-right">
           <Link
-            href={`/cabins/${id}`}
+            href={`/capsulas/${id}`}
             className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
           >
             Details & reservation &rarr;
--- app/_lib/data_service.js
+++ app/_lib/data_service.js
@@ -12,7 +12,7 @@ export async function getCabin(id) {
     .single();
 
   // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
+  await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
