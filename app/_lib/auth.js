dc3a597aeb2c5e9124d12c382d1119b9e313738c
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2024-11-23 19:25:18 -0500

    Usuarios con supabase
    Por medio de la sesión guardamos los usuarios nuevos en nuestra base de dtaos de supabase

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -13,7 +13,23 @@ const authConfig = {
     authorized({auth, request}){
       return !!auth?.user
     },
-  },
+    async signIn({user, account, profile}){
+      try{
+      const existingGuest = await getGuest(user.email);
+
+      if (!existingGuest)
+        await createGuest({ email: user.email, fullName: user.name });
+      return true;
+      }catch{
+       return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
+    },
   pages:{
     signIn: "/login",
   },
