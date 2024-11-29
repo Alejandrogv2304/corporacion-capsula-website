7a18df65592b55af50daedfaf48214d9cd37811f
Alejandrogv2304 <alejandrogv.1011@gmail.com>
2024-11-29 11:12:23 -0500

    Arreglo bug cabinas
    Bug cabinas que no cargan

--- app/capsulas/[capsulaId]/page.js
+++ app/capsulas/[capsulaId]/page.js
@@ -18,7 +18,7 @@ export async function generateMetadata({ params }) {
 export async function generateStaticParams() {
   const cabins = await getCabins();
 
-  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+  const ids = cabins.map((cabin) => ({ capsulaId: String(cabin.id) }));
 
   return ids;
 }
